ed8e0ea46df1f9dff000b89be9b0f5227755d102
bintangahmad15 <bintangahmad1510@gmail.com>
2025-06-16 08:14:26 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const less = require('gulp-less');
 const rename = require('gulp-rename');
-
 // Compile SCSS
 gulp.task('scss', function () {
-  return gulp
-    .src('src/style.scss')
+  return gulp.src('src/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('style.css'))
     .pipe(gulp.dest('dist'));
@@ -14,17 +12,17 @@ gulp.task('scss', function () {
 
 // Compile LESS
 gulp.task('less', function () {
-  return gulp
-    .src('src/style.less')
+  return gulp.src('src/style.less')
     .pipe(less())
     .pipe(rename('style-from-less.css'))
     .pipe(gulp.dest('dist'));
 });
 
-// Watch files
+// Watch task
 gulp.task('watch', function () {
   gulp.watch('src/**/*.scss', gulp.series('scss'));
   gulp.watch('src/**/*.less', gulp.series('less'));
 });
 
 gulp.task('default', gulp.series('scss', 'less', 'watch'));
+
